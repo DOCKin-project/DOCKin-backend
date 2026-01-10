c73c10df9e54e96349214ddb5b1ae5851ffe20e8
Khyojae <khjae201@gmail.com>
2026-01-10 21:54:04 +0900

    chore: ChatRoomController 어노테이션 변경 (createRoom)

--- src/main/java/com/DOCKin/controller/ChatRoomController.java
+++ src/main/java/com/DOCKin/controller/ChatRoomController.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class ChatRoomController {
     @Operation(summary="채팅방 생성", description = "새로운 채팅방을 생성함")
     @PostMapping("/room")
-    public ResponseEntity<ChatRoomRequestDto> createRoom(@RequestParam String name){
+    public ResponseEntity<ChatRoomRequestDto> createRoom(@RequestBody String name){
         ChatRoomRequestDto room = new ChatRoomRequestDto();
         return ResponseEntity.ok(new ChatRoomRequestDto());
     }
