3f61565ea22b4902408545f8e98bc6e8428fece6
Khyojae <khjae201@gmail.com>
2026-01-16 14:46:00 +0900

    feat: ChatRoomService 추가

--- src/main/java/com/DOCKin/service/ChatRoomService.java
+++ src/main/java/com/DOCKin/service/ChatRoomService.java
@@ -1,7 +1,12 @@
 package com.DOCKin.service;
 
 import com.DOCKin.dto.chat.ChatRoomRequestDto;
+import com.DOCKin.global.error.BusinessException;
+import com.DOCKin.global.error.ErrorCode;
+import com.DOCKin.model.Chat.ChatMembers;
 import com.DOCKin.model.Chat.ChatRooms;
+import com.DOCKin.model.Member.Member;
+import com.DOCKin.repository.ChatMembersRepository;
 import com.DOCKin.repository.ChatMessagesRepository;
 import com.DOCKin.repository.ChatRoomsRepository;
 import com.DOCKin.repository.MemberRepository;
@@ -10,22 +15,50 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ChatRoomService {
-    private final ChatMessagesRepository chatMessagesRepository;
     private final ChatRoomsRepository chatRoomsRepository;
+    private final ChatMembersRepository chatMembersRepository;
     private final MemberRepository memberRepository;
 
+    //채팅방 개설
     @Transactional
-    public Integer createChatRoom(ChatRoomRequestDto dto){
-        ChatRooms savedRoom = ChatRooms.builder()
+    public Integer createChatRoom(ChatRoomRequestDto dto,String creatorId){
+        ChatRooms rooms = ChatRooms.builder()
                 .roomName(dto.getRoom_name())
                 .isGroup(dto.getIs_group())
                 .creatorId(dto.getCreatorId())
                 .build();
 
-        addMemberToRoom(savedRoom, dto.getCrea)
+        ChatRooms savedRoom = chatRoomsRepository.save(rooms);
+
+        //방장을 첫 번째 멤버로 저장
+        saveMember(savedRoom,creatorId);
+
+        //초대받은 참여자들 저장
+        if(dto.getParticipantIds()!=null){
+            dto.getParticipantIds().stream()
+                    .filter(userId -> !userId.equals(creatorId))
+                    .forEach(userId->saveMember(savedRoom,userId));
+
+        }
+        return  savedRoom.getRoomId();
+    }
+
+
+    private void saveMember(ChatRooms rooms, String userId){
+        Member memberEntity = memberRepository.findById(userId)
+                .orElseThrow(()->new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        ChatMembers chatMember = ChatMembers.builder()
+                .chatRooms(rooms)
+                .member(memberEntity)
+                .joinedAt(LocalDateTime.now())
+                .build();
+        chatMembersRepository.save(chatMember);
     }
 }
