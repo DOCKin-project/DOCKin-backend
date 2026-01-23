7c628334ac177bd121c4f96e7a753dc8bdc3ac12
Khyojae <khjae201@gmail.com>
2026-01-23 22:38:33 +0900

    feat: 댓글 controller 추가

--- src/main/java/com/DOCKin/controller/WorkLogCommentController.java
+++ src/main/java/com/DOCKin/controller/WorkLogCommentController.java
@@ -5,14 +5,18 @@ import com.DOCKin.dto.WorkLogs.comment.CommentResponseDto;
 import com.DOCKin.dto.WorkLogs.comment.CommentUpdateRequestDto;
 import com.DOCKin.global.security.auth.CustomUserDetails;
 import com.DOCKin.model.WorkLogs.Comment;
+import com.DOCKin.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name="작업일지 댓글 기능",description = "작업일지에 댓글을 달 수 있음")
 @Slf4j
@@ -20,32 +24,43 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/work-logs/{logId}/comments")
 public class WorkLogCommentController {
+    private final CommentService commentService;
 
     @Operation(summary = "관리자 댓글 작성",description = "관리자가 근로자 작업일지 피드백을 해줌")
     @PostMapping
     public ResponseEntity<CommentResponseDto> postComment(@AuthenticationPrincipal CustomUserDetails customUserDetails
-    , @Valid @RequestBody CommentCreateRequestDto dto, String logId){
-        return ResponseEntity.ok(null);
+    , @Valid @RequestBody CommentCreateRequestDto dto,@PathVariable Long logId){
+        String userId =customUserDetails.getMember().getUserId();
+        CommentResponseDto comment =commentService.createComment(logId,userId,dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(comment);
     }
 
     @Operation(summary="관리자 댓글 수정",description = "관리자가 자기가 쓴 댓글을 수정함")
-    @PutMapping
+    @PutMapping("/{commentId}")
     public ResponseEntity<CommentResponseDto> putComment(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                                             @Valid @RequestBody CommentUpdateRequestDto dto,
-                                                            @PathVariable Long logId){
-        return ResponseEntity.ok(null);
+                                                            @PathVariable Long logId,
+                                                         @PathVariable Long commentId){
+        String userId =customUserDetails.getMember().getUserId();
+        CommentResponseDto comment = commentService.updateComment(logId,userId,commentId,dto);
+        return ResponseEntity.ok(comment);
     }
 
     @Operation(summary="관리자 댓글 조회",description = "관리자가 쓴 댓글을 볼 수 있음")
     @GetMapping
-    public ResponseEntity<CommentResponseDto> getComment(@AuthenticationPrincipal CustomUserDetails customUserDetails){
-        return ResponseEntity.ok(null);
+    public ResponseEntity<List<CommentResponseDto>> getComment(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                               @PathVariable Long lodId){
+        List<CommentResponseDto> comment = commentService.readComment(lodId);
+        return ResponseEntity.ok(comment);
     }
 
     @Operation(summary ="관리자 댓글 삭제",description = "관리자가 쓴 댓글을 삭제할 수 있음")
-    @DeleteMapping
+    @DeleteMapping("/{commentId}")
     public ResponseEntity<Void> deleteComment(@AuthenticationPrincipal CustomUserDetails customUserDetails,
-                                                            String userId){
+                                                            @PathVariable Long logId,
+                                              @PathVariable Long commentId){
+        String userId = customUserDetails.getMember().getUserId();
+        commentService.deleteComment(logId,commentId,userId);
         return ResponseEntity.noContent().build();
     }
 }
